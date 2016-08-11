71e9cebcceaec19b3eb62a13e151f47e1fef1959
codephillip <codephillip@gmail.com>
2016-08-11 23:35:31 +0300

    refactored code

--- myapp/views.py
+++ myapp/views.py
@@ -14,29 +14,24 @@ def index(request):
     })
 
 
-def result(request, qn_id=None):
-    # gets access to global variable
+def queryDb(qn_id):
     global score
-
     try:
         exam = Exam.objects.get(id=int(qn_id) + 1)
     except Exception:
-        print(str(Exception) + 'BUG')
+        print(str(Exception))
         score = 0
         exam = Exam.objects.get(id=5)
+    finally:
+        return exam
 
+
+def result(request, qn_id=None):
+    # gets access to global variable
+    global score
+    exam = queryDb(qn_id)
     # qn_id is a unicode, convert it to int
     print(int(qn_id) + 1)
-
-    # if request.method == 'POST':
-    #     if request.POST['answer'] == exam.answer:
-    #         score += 1
-    #
-    #     print("SCORE " + str(score))
-    #     return render(request, 'result.html', {
-    #         'exam': exam,
-    #         'score': score
-    #     })
 
     if request.method == 'POST':
         form = ExamForm(request.POST)
@@ -44,11 +39,8 @@ def result(request, qn_id=None):
             # returns the actual data from the form
             data = form.cleaned_data
             # returns the structure of the form
-            print(form['answer'])
-
-            if data['answer'] == exam.answer:
-                score += 1
-
+            # print(form['answer'])
+            check_result(data, qn_id)
         print("SCORE " + str(score))
         return render(request, 'result.html', {
             'exam': exam,
@@ -61,5 +53,11 @@ def result(request, qn_id=None):
             'exam': exam,
             'score': score,
             'form': form
-
         })
+
+
+def check_result(data, qn_id):
+    global score
+    exam2 = Exam.objects.get(id=qn_id)
+    if data['answer'] == exam2.answer:
+        score += 1
