17d0b924f4b400f6edb2c1e2165a059263d5d041
codephillip <codephillip@gmail.com>
2016-08-12 01:00:51 +0300

    added TimeTable to models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 
-from myapp.models import Exam
+from myapp.models import Exam, Room, Subject, TimeTable
 
 
 class ExamAdmin(admin.ModelAdmin):
     model = Exam
     list_display = ('id', 'question', 'answer')
 
+
+class RoomAdmin(admin.ModelAdmin):
+    model = Room
+    list_display = ('id', 'room_name')
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    model = Subject
+    list_display = ('id', 'subject_name')
+
+
+class TimeTableAdmin(admin.ModelAdmin):
+    model = TimeTable
+    list_display = ('id', 'sub_id', 'room_id', 'time', 'date')
+
+    def sub_id(self, instance):
+        return instance.subject.subject_name
+
+    def room_id(self, instance):
+        return instance.room.room_name
+
+
 admin.site.register(Exam, ExamAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(TimeTable, TimeTableAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,27 @@ from django.db import models
 class Exam(models.Model):
     question = models.CharField(max_length=255)
     answer = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.question
+
+
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.subject_name
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.room_name
+
+
+class TimeTable(models.Model):
+    sub_id = models.ForeignKey(Subject)
+    room_id = models.ForeignKey(Room)
+    time = models.TimeField()
+    date = models.DateField()
