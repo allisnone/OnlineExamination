2fc3ee90afae95c8110433fce65102fdc3ab9009
codephillip <codephillip@gmail.com>
2016-08-11 19:57:19 +0300

    added changing questions

--- myapp/views.py
+++ myapp/views.py
@@ -9,9 +9,15 @@ def index(request):
 
 
 def result(request, qn_id):
+    counter = 0
+    if counter >= 2:
+        counter = 0
+    else:
+        counter += 1
+    qn = ['what is your name?', 'when did NASA step on the moon?', 'who stepped on the moon?']
     if request.method == 'POST':
         return render(request, 'result.html', {
-            'qn_id': request.POST['first']
+            'qn_id': qn[counter]
         })
 
     return render(request, 'result.html', {
